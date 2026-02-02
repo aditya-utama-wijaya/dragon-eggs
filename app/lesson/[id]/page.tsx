cd3c5e32c97921e1dbaa41c8ae35251874adceda
unknown <fransiscus_au_w@telkomsel.co.id>
2026-02-02 16:46:21 +0700

    Remove static export - use native Next.js on Vercel

--- app/lesson/[id]/page.tsx
+++ app/lesson/[id]/page.tsx
@@ -11,13 +11,6 @@ import { useUserProgress } from '@/context/UserProgressContext';
 import lessonsData from '@/data/lessons.json';
 import styles from './page.module.css';
 
-// Generate static params for all lessons
-export function generateStaticParams() {
-  return lessonsData.lessons.map((lesson) => ({
-    id: lesson.id,
-  }));
-}
-
 export default function LessonPage() {
   const params = useParams();
   const router = useRouter();
